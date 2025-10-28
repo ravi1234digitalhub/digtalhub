fb2ab43b3550ee4f73f66f48cf0edc0d373dd180
ravi1234digitalhub <bhelwa143@gmail.com>
2025-10-28 11:45:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("digital-hub-cache").then(cache => {
-      return cache.addAll(["./", "./index.html"]);
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('digital-hub-v1').then(cache => {
+      return cache.addAll(['./', './index.html']);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
